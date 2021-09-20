e4b57a43e77d7a46a1022894d1665038b670bdbf
Lucas Tagliani <lucas.tagliani@outlook.com>
2021-09-20 00:06:20 -0300

    feat: add /teams path with mock data

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const staticTeams = require('./api/teams/static-data/teams.json')
+
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/teams', (req, res) => {
+  res.send(staticTeams)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
