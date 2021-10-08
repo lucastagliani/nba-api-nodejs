15e46312dcdb87ffb4218ee8dc27583515e0c48f
Lucas Tagliani <lucas.tagliani@outlook.com>
2021-10-08 00:43:53 -0300

    test(teams): all tests passing

--- src/api/teams/service.test.js
+++ src/api/teams/service.test.js
@@ -1,22 +1,48 @@
 import axios from 'axios'
-// import teams from './static-data/teams.json'
+import constants from '../../constants.js'
+import staticTeams from './static-data/teams.json'
+// import * as constants from '../../constants.js'
 
-jest.mock(axios)
+import { getTeams } from './service.js'
+
+jest.mock('axios')
+jest.mock('../../constants.js', () => ({
+  get features() {
+    return { HIT_REAL_API_TEAMS: false }
+  },
+}))
 
 describe('teams service', () => {
-  describe('when HIT_REAL_API_TEAMS is ON', () => {
-    it('should call /teams/league/sacramento', () => {
-      axios.get.mockResolvedValueOnce({})
+  const expectedData = {
+    api: {
+      filters: expect.any(Array), message: 'GET teams/league/sacramento', results: 34, status: 200, teams: expect.any(Array),
+    },
+  }
 
-      expect(false).toBeFalsy()
+  describe('when HIT_REAL_API_TEAMS is ON', () => {
+    it('should call /teams/league/sacramento', async () => {
+      const mySpy = jest.spyOn(constants, 'features', 'get')
+      mySpy.mockReturnValue({ HIT_REAL_API_TEAMS: true })
+
+      axios.request.mockImplementation(() => Promise.resolve({ data: staticTeams }))
+      const result = await getTeams()
+      expect(axios.request).toHaveBeenCalledTimes(1)
+      expect(result).toMatchObject(expectedData)
     })
   })
 
   describe('when HIT_REAL_API_TEAMS is OFF', () => {
-    it('should not call /teams/league/sacramento', () => {
-      axios.get.mockResolvedValueOnce({})
+    it('should not call /teams/league/sacramento', async () => {
+      const mySpy = jest.spyOn(constants, 'features', 'get')
+      mySpy.mockReturnValue({ HIT_REAL_API_TEAMS: false })
 
-      it(false).toBeFalsy()
+      const result = await getTeams()
+      expect(axios.request).not.toHaveBeenCalled()
+      expect(result).toMatchObject(expectedData)
     })
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
   })
 })
